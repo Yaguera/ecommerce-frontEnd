fa04324509eca042d774d46625ef1dadc45dfc5f
Yaguera <ya-gotre@hotmail.com>
2024-11-29 20:47:48 -0300

    feat: add CartList and CartItem components

---
+++ app/components/CartList.tsx
@@ -0,0 +1,34 @@
+import CartItem from "./CartItem";
+
+interface CartListProps {
+  cart: {
+    id: string;
+    nome: string;
+    price: number;
+    quantity: number;
+    imageUrl: string;
+  }[];
+  updateQuantity: (id: string, newQuantity: number) => void;
+  removeFromCart: (id: string) => void;
+}
+
+const CartList: React.FC<CartListProps> = ({
+  cart,
+  updateQuantity,
+  removeFromCart,
+}) => {
+  return (
+    <div className="flex justify-center items-center gap-3">
+      {cart.map((item) => (
+        <CartItem
+          key={item.id}
+          {...item}
+          updateQuantity={updateQuantity}
+          removeFromCart={removeFromCart}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default CartList;
